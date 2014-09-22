f66f086ee40452b77701e03d29eb4a60b211b8e1
Robert Warren <grindnmosh@gmail.com>
2014-09-21 23:18:04 -0700

    added add button to main page and functionality to load form.

--- Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/MyActivity.java
+++ Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/MyActivity.java
@@ -1,7 +1,9 @@
 package com.grinddesign.geogrind;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -28,8 +30,10 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.add) {
+            Intent addNew = new Intent(this, Form.class);
+
+            this.startActivity(addNew);
         }
         return super.onOptionsItemSelected(item);
     }
