ed524cf182003e1446e514541d18506da319b1d2
Robert Warren <grindnmosh@gmail.com>
2014-09-22 22:27:29 -0700

    Being told to update already current google services to run app. It is an issue asked about multiple times and have not found anyone with solution.

---
+++ Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/MainFrag.java
@@ -0,0 +1,36 @@
+package com.grinddesign.geogrind;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class MainFrag extends MapFragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/MyActivity.java
+++ Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/MyActivity.java
@@ -14,6 +14,9 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        MainFrag frag = new MainFrag();
+        getFragmentManager().beginTransaction().replace(R.id.container, frag).commit();
     }
 
 
