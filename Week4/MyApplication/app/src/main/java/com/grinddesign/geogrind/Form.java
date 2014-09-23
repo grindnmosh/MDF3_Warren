f10bf42f288b0acd645a8a64722474080ebcea49
Robert Warren <grindnmosh@gmail.com>
2014-09-22 19:45:27 -0700

    Location services functioning in form and auto populates image name, date, lat and long. added button I will use to pass data to load into map and detail pages.

--- Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/Form.java
+++ Week4/MyApplication/app/src/main/java/com/grinddesign/geogrind/Form.java
@@ -1,32 +1,51 @@
 package com.grinddesign.geogrind;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class Form extends Activity {
+public class Form extends Activity implements LocationListener {
 
     private static final int REQUEST_TAKE_PICTURE = 0x01001;
+    private static final int REQUEST_ENABLE_GPS = 0x02001;
 
     Button cam;
+    Button saveButt;
     ImageView img;
     Uri mImageUri;
+
+    TextView imgName;
+    TextView mLatitude;
+    TextView mLongitude;
+    TextView imgDate;
+
+    String imageName;
+    String imageDate;
+
+    LocationManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +53,21 @@ public class Form extends Activity {
         setContentView(R.layout.activity_form);
 
         cam = (Button) findViewById(R.id.camera);
+        saveButt = (Button) findViewById(R.id.saveMe);
         img = (ImageView) findViewById(R.id.savedImage);
+        imgName = (TextView) findViewById(R.id.imageName);
+        mLatitude = (TextView) findViewById(R.id.latitude);
+        mLongitude = (TextView) findViewById(R.id.longitude);
+        imgDate = (TextView) findViewById(R.id.dateText);
+        mManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+
+        saveButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create intent to pass data to load into map
+            }
+        });
         cam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,10 +75,75 @@ public class Form extends Activity {
                 mImageUri = getOutputUrl();
                 if(mImageUri != null) {
                     cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+
                 }
                 startActivityForResult(cameraIntent, REQUEST_TAKE_PICTURE);
+
+                enableGps();
             }
         });
+
+    }
+
+    private void enableGps() {
+        if(mManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            mManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, (android.location.LocationListener) this);
+
+            Location loc = mManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(loc != null) {
+                mLatitude.setText("lat = " + loc.getLatitude());
+                mLongitude.setText("long = " + loc.getLongitude());
+            }
+
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("GPS Unavailable")
+                    .setMessage("Please enable GPS in the system settings.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivityForResult(settingsIntent, REQUEST_ENABLE_GPS);
+                        }
+
+                    })
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //enableGps();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mManager.removeUpdates((android.location.LocationListener) this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLatitude.setText("" + location.getLatitude());
+        mLongitude.setText("" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
@@ -55,16 +153,16 @@ public class Form extends Activity {
             if (data == null) {
                 img.setImageBitmap(BitmapFactory.decodeFile(mImageUri.getPath()));
                 addImageToGallery(mImageUri);
+                //enableGps();
             } else {
                 img.setImageBitmap((Bitmap) data.getParcelableExtra("data"));
             }
-
-
         }
     }
 
     private Uri getOutputUrl() {
-        String imageName = new SimpleDateFormat("MMddyyyy_HHmmss").format(new Date(System.currentTimeMillis()));
+        imageName = new SimpleDateFormat("MMddyyyy_HHmmss").format(new Date(System.currentTimeMillis())) + "_GeoGrind";
+        imageDate = new SimpleDateFormat("MM/dd/yyyy").format(new Date(System.currentTimeMillis()));
         File imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         // Creating our own folder in the default directory.
         File appDir = new File(imageDir, "CameraExample");
@@ -76,6 +174,8 @@ public class Form extends Activity {
             e.printStackTrace();
             return null;
         }
+        imgName.setText("Image Name = " + imageName);
+        imgDate.setText("Date = " + imageDate);
         return Uri.fromFile(image);
     }
 
